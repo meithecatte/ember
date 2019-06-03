5361d6e98144e169e8f9a6f940af23c0bf64a1d0
Jakub Kądziołka <kuba@kadziolka.net>
2019-06-03 17:19:40 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -ev
+rm -rf gen
+mkdir gen
+nasm mbrsh.s -o gen/mbrsh.bin
+truncate -s 64M gen/fs.img
+mkfs.fat -F 32 gen/fs.img
+mcopy -i gen/fs.img fs/* ::
+truncate -s $(($(stat -c %s gen/fs.img) + 512)) gen/mbrsh.img
+sfdisk --quiet --no-reread --no-tell-kernel gen/mbrsh.img <<EOF
+label: dos
+unit: sectors
+1
+EOF
+dd if=gen/mbrsh.bin of=gen/mbrsh.img conv=notrunc
+dd if=gen/fs.img    of=gen/mbrsh.img conv=notrunc bs=512 seek=1 status=none
+[ "$1" = "run" ] && qemu-system-i386 -curses -drive format=raw,file=gen/mbrsh.img
---
+++ mbrsh.s
@@ -0,0 +1,192 @@
+; memory map:
+; [0000; 0400) IVT
+; [0400; 0500) BDA
+; [    ; 7c00) stack
+; [7c00; 7e00) MBR
+; [7e00; 8000) BPB sector
+
+; FILE structure:
+; .buffer: ds 0x200    ; (offset 0)
+; .initial_cluster: dd ; (offset 0x200)
+; .current_cluster: dd ; (offset 0x204)
+; .offset: dw          ; (offset 0x208)
+
+%define FILE_initial_cluster 0x200
+%define FILE_current_cluster 0x204
+%define FILE_offset          0x208
+%define sizeof_FILE          0x20a
+
+%define BPB_loadaddr    0x7e00
+%define BPB_SecPerClus  BPB_loadaddr + 13 ; db
+%define BPB_ResvdSecCnt BPB_loadaddr + 14 ; dw
+%define BPB_NumFATs     BPB_loadaddr + 16 ; db
+%define BPB_FATSz32     BPB_loadaddr + 36 ; dd
+org 0x7c00
+bits 16
+
+; Some BIOSes start with CS=07c0. Make sure this does not wreak havoc
+	jmp 0:start
+
+ivtptr:
+%define i_biosdisp 0x10
+%define i_biosdisk 0x13
+%define i_bioskbd  0x16
+
+%define i_readline 0x80
+	dw readline
+%define i_partread 0x81
+	dw partread
+%define i_readcluster 0x82
+	dw readcluster
+%define i_readopen 0x83
+	dw readopen
+.end:
+
+start:
+	cli
+	xor cx, cx
+	mov ds, cx
+	mov es, cx
+	mov ss, cx
+	mov sp, 0x7c00
+	mov [dopacket.diskload+1], dl
+	sti
+
+	mov di, 0x200
+	mov si, ivtptr
+	mov cl, ivtptr.end - ivtptr
+.ivtloop:
+	movsw
+	; it's not necessary to clear the upper part of EAX now, but it saves bytes
+	; compared to doing it again before the BPB read
+	xor eax, eax
+	stosw
+	loop .ivtloop
+
+; Read the BPB
+	mov di, 0x7e00
+	xor eax, eax
+	int i_partread
+
+	; eax = 0
+	mov al, byte[BPB_NumFATs]
+	mul dword[BPB_FATSz32]
+	movzx ebx, byte[BPB_SecPerClus]
+	add bx, bx
+	sub eax, ebx
+	mov bx, word[BPB_ResvdSecCnt]
+	add eax, ebx
+	mov [readcluster.offset+2], eax
+
+	mov bx, 3
+	mov cl, 0
+	mov di, 0x8000
+	int i_readcluster
+
+	cli
+	hlt
+
+shell:
+	xor bx, bx
+	mov ax, 0x0e00 + '>'
+	int i_biosdisp
+
+	mov di, 0x600
+	int i_readline
+	; TODO
+
+; Read a line of text, store at ES:DI. Returns end in DI.
+readline:
+	push ax
+	push bx
+	push bp ; some BIOSes thrash BP on scroll -RBIL
+.loop:
+	mov ah, 0
+	int i_bioskbd
+
+	cmp al, 8
+	jne short .nobackspace
+	dec di
+	db 0xb4 ; load the opcode of the stosb to AH to skip it
+.nobackspace:
+	stosb
+
+	mov ah, 0x0e
+	xor bx, bx
+	int i_biosdisp
+	cmp al, 13
+	jne short .loop
+	mov al, 10
+	int i_biosdisp
+	pop bp
+	pop bx
+	pop ax
+	dec di
+	iret
+
+; Prepare a disk operation packet.
+; Input:
+;  eax = LBA (partition-relative)
+;  es:di = operation buffer
+; Output:
+;  dl = drive number
+;  ds:si = disk packet pointer (0000:7DF0)
+dopacket:
+	push cs
+	pop ds
+	add eax, [0x7c00+0x1be+8]
+	mov si, 0x7df0
+	mov dword[si], 0x10010
+	mov [si+4], di
+	mov [si+6], es
+	mov [si+8], eax
+	xor eax, eax
+	mov [si+12], eax
+.diskload:
+	mov dl, 0 ; overwritten during init
+	ret
+
+; Read sector 
+; Input: as in dopacket
+; Stops execution on erro
+partread:
+	pusha
+.after_pusha
+	push ds
+	call dopacket
+	mov ah, 0x42
+	int i_biosdisk
+	jc short error
+	pop ds
+	popa
+	iret
+
+error:
+	mov ax, 0x0e21
+	xor bx, bx
+	int i_biosdisp
+	cli
+	hlt
+
+; Read a sector of a cluster.
+; Input:
+;  ebx = cluster number
+;  cl = sector offset within cluster
+;  es:di = buffer (512 bytes)
+readcluster:
+	pusha
+	movzx eax, byte[cs:BPB_SecPerClus]
+	mul ebx
+.offset:
+	add eax, 0xaaaaaaaa ; overwritten during init
+	movzx ecx, cl
+	add eax, ecx
+	jmp short partread.after_pusha
+
+; Open a file for reading.
+; Input:
+;  DS:SI = file name
+;  ES:DI = file structure
+; Output:
+;  carry flag set if not found
+readopen:
