f6b0e8f3785853b9b453dc8e67eb91cdb112b2d6
Jakub Kądziołka <kuba@kadziolka.net>
2019-06-21 02:59:36 +0200

    Remove the filesystem stuff from the build script

--- build.sh
+++ build.sh
@@ -2,16 +2,5 @@
 set -ev
 rm -rf gen
 mkdir gen
-nasm mbrsh.s -o gen/mbrsh.bin -l gen/mbrsh.lst
-truncate -s 64M gen/fs.img
-mkfs.fat -F 32 gen/fs.img
-mcopy -i gen/fs.img fs/* ::
-truncate -s $(($(stat -c %s gen/fs.img) + 512)) gen/mbrsh.img
-sfdisk --quiet --no-reread --no-tell-kernel gen/mbrsh.img <<EOF
-label: dos
-unit: sectors
-1
-EOF
-dd if=gen/mbrsh.bin of=gen/mbrsh.img conv=notrunc
-dd if=gen/fs.img    of=gen/mbrsh.img conv=notrunc bs=512 seek=1 status=none
+nasm mbrsh.s -o gen/mbrsh.img -l gen/mbrsh.lst
 [ "$1" = "run" ] && qemu-system-i386 -curses -drive format=raw,file=gen/mbrsh.img
